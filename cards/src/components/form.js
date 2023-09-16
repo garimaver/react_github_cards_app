745a6682335fe95bfee60628db17bc8640969c36
garima verma <114860130+garimaver@users.noreply.github.com>
2023-09-16 12:55:29 +0530

    react form.js

--- cards/src/components/form.js
+++ cards/src/components/form.js
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 
 function Form() {
     const [name,setName]=useState("");
+    function handleSubmit() {}
   return (
     <div>
+        <form onSubmit={handleSubmit}>
       <input type={"text"} 
       onChange={(event)=>{
         setName(event.target.value);
     }}
         value = {name}
         ></input>
-      <button>Add</button>
+      <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
