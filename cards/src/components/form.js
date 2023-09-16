e4b713431e207c4496db35afa8d6eb6db9468ed6
garima verma <114860130+garimaver@users.noreply.github.com>
2023-09-16 13:11:36 +0530

    added axios

--- cards/src/components/form.js
+++ cards/src/components/form.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 
 function Form() {
     const [name,setName]=useState("");
-    function handleSubmit() {}
+    function handleSubmit(event) {
+        event.preventDefault();
+        //console.log(name);
+        const resp = await
+    }
   return (
     <div>
         <form onSubmit={handleSubmit}>
       <input type={"text"} 
       onChange={(event)=>{
         setName(event.target.value);
-    }}
+  }}
         value = {name}
         ></input>
       <button type="submit">Add</button>
